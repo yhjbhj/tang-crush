1c6ae616db49d856e8c91ad9a3418d9a3ccebb78
yhjbhj <86763726+yhjbhj@users.noreply.github.com>
2021-07-01 13:23:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,6 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t
+    messLink: 'https://m.me/tranvan.truong.79656'
    
 }
